eec6731e6813e65ce6b48da2660c8f463f9a1a4f
llmiseq <152598429+llmiseq@users.noreply.github.com>
2024-03-08 21:38:56 +0100

    dodano otwieranie gui poleceniem

--- src/main/java/com/jakub/artefakty/Artefakty.java
+++ src/main/java/com/jakub/artefakty/Artefakty.java
@@ -2,19 +2,21 @@ package com.jakub.artefakty;
 
 import mc.thelblack.custominventory.CInventoryManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
 public final class Artefakty extends JavaPlugin {
 
     static CInventoryManager inventoryManager;
+    private getArtefaktyInventory artefaktyInventory;
 
     @Override
     public void onEnable() {
         inventoryManager = new CInventoryManager(this);
-        getCommand("trofeum").setExecutor(new GuiCommands());
+        artefaktyInventory = new getArtefaktyInventory();
+        getCommand("trofeum").setExecutor(new GuiCommands(artefaktyInventory));
     }
 
     @Override
     public void onDisable() {
-
-
+        // Kod do wykonania podczas wyłączania pluginu
     }
 }
--- src/main/java/com/jakub/artefakty/GuiCommands.java
+++ src/main/java/com/jakub/artefakty/GuiCommands.java
@@ -1,8 +1,6 @@
 package com.jakub.artefakty;
 
-import com.destroystokyo.paper.event.block.BeaconEffectEvent;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandException;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -10,15 +8,25 @@ import org.jetbrains.annotations.NotNull;
 
 public class GuiCommands implements CommandExecutor {
 
+    GuiCommands() {
+
+    }
+    private getArtefaktyInventory artefaktyInventory;
+
+    public GuiCommands(getArtefaktyInventory artefaktyInventory) {
+        this.artefaktyInventory = artefaktyInventory;
+    }
+
+
 
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
 
         if (commandSender instanceof Player) {
-             Player player = (Player) commandSender;
-             player.sendMessage("§aOtwieram trofea...");
-
+            Player player = (Player) commandSender;
+            player.sendMessage("§aOtwieram trofea...");
+            player.openInventory(artefaktyInventory.getArtefaktyInventory(1, 2, 3, 4, 5, 6));
         }
-        return false;
+        return true;
     }
 }
